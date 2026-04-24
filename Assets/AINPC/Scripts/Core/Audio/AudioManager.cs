befa855649468c0adc12fe28dd75e09240f45c65
Ashutosh B. <52480678+icode365@users.noreply.github.com>
2026-04-24 17:29:41 +0530

    added button click audio for button prefabs

--- Assets/AINPC/Scripts/Core/Audio/AudioManager.cs
+++ Assets/AINPC/Scripts/Core/Audio/AudioManager.cs
@@ -1,16 +1,57 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class AudioManager : MonoBehaviour
+namespace AINPC.Scripts.Core.Audio
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class AudioManager : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private AudioClip genericButtonClickSfx;
+        [SerializeField] private AudioSource buttonClickSource;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        void Start()
+        {
+            Initialize();
+        }
+
+        private void OnDestroy()
+        {
+            RemoveAllButtonListners();
+        }
+
+        public void Initialize()
+        {
+            AssignGenericButtonClickSfx();
+        }
+
+        private void AssignGenericButtonClickSfx()
+        {
+            var buttons = FindObjectsOfTypeAll(typeof(Button));
+            Debug.Log("Buttons Count :" + buttons.Length);
+            foreach (var item in buttons)
+            {
+                var button = item as Button;
+                button.onClick.AddListener(PlayGenericButtonClickSfx);
+            }
+        }
+
+        private void RemoveAllButtonListners()
+        {
+            var buttons = FindObjectsOfTypeAll(typeof(Button));
+            foreach (var item in buttons)
+            {
+                var button = item as Button;
+                button.onClick.RemoveListener(PlayGenericButtonClickSfx);
+            }
+        }
+
+        private void PlayGenericButtonClickSfx()
+        {
+            buttonClickSource.clip = genericButtonClickSfx;
+            
+            if (buttonClickSource.isPlaying)
+                buttonClickSource.Stop();
+            buttonClickSource.Play();
+        }
     }
 }
--- Assets/AINPC/Scripts/UI/HoverAnimatedBrewButton.cs
+++ Assets/AINPC/Scripts/UI/HoverAnimatedBrewButton.cs
@@ -1,23 +1,58 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(AudioSource))]
 public class HoverFillButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private Image fillImage;
 
-    [Header("Tween Settings")]
-    [SerializeField] private float hoverDuration = 0.35f;
+    [SerializeField] private AudioClip hoverSfx;
+    [SerializeField] private AudioSource audioSource;
+
+    [Header("Tween Settings")] [SerializeField]
+    private float hoverDuration = 0.35f;
+
     [SerializeField] private float exitDuration = 0.12f;
 
     private int _tweenId = -1;
     private bool _shouldAnimate = false;
-    
+
     private void Awake()
     {
         fillImage.fillAmount = 0;
     }
 
+    private void Start()
+    {
+        SetupAudioSource();
+    }
+    
+    private void SetupAudioSource()
+    {
+        if (audioSource == null)
+            audioSource = AddAudioSourceComponent();
+
+        audioSource.playOnAwake = false;
+        audioSource.loop = false;
+        audioSource.clip = hoverSfx;
+    }
+
+    private void PlayHoverSfx()
+    {
+        StopHoverSfx();
+        audioSource.Play();
+    }
+    
+    private void StopHoverSfx()
+    {
+        if (audioSource.isPlaying)
+            audioSource.Stop();
+    }
+
+    private AudioSource AddAudioSourceComponent() => gameObject.AddComponent<AudioSource>();
+    
     public void SetShouldAnimate(bool shouldAnimate)
     {
         _shouldAnimate = shouldAnimate;
@@ -26,21 +61,23 @@ public class HoverFillButton : MonoBehaviour, IPointerEnterHandler, IPointerExit
     public void OnPointerEnter(PointerEventData eventData)
     {
         TryAnimate(1f);
+        PlayHoverSfx();
     }
-    
+
     public void OnPointerExit(PointerEventData eventData)
     {
         TryAnimate(0f);
+        StopHoverSfx();
     }
 
     private void TryAnimate(float target)
     {
         // todo : add this back in a way that, when we hover over the button, the animation should not play unless and untill the slots are filled
         // if (!_shouldAnimate) return;
-        
+
         AnimateFill(target, hoverDuration, LeanTweenType.easeOutQuad);
     }
-    
+
     private void AnimateFill(float target, float duration, LeanTweenType ease)
     {
         if (fillImage == null)
